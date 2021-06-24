757b39775886a7b69f7cf961e57ac15bf70c7a6c
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2021-06-24 17:51:31 +0200

    timestamp

--- main.py
+++ main.py
@@ -3,34 +3,46 @@ import time
 import requests
 from bs4 import BeautifulSoup
 from playsound import playsound
+from datetime import datetime
 
 
+def getTime():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
 
 # link for extract html data
 def getdata(url):
     r = requests.get(url)
     return r.text
 
-while True:
-
-        htmldata = getdata("https://refactor.jp/chivalry/?serverId=1491190")
-        soup = BeautifulSoup(htmldata, 'html.parser')
-        data = ''
-        onlinePlayers = soup.find_all("td")
-        if len(onlinePlayers) > 0:
-            print(str(len(onlinePlayers)/3) + " players")
-            playsound('audio.mp3', False)
-            var = input("Please ENTER to stop searching or other key to continue: ")
-            if len(var) == 0:
-                print("STOPPING")
-                break
+def check():
+    while True:
+        try:
+            htmldata = getdata("https://refactor.jp/chivalry/?serverId=1491190")
+            soup = BeautifulSoup(htmldata, 'html.parser')
+            data = ''
+            onlinePlayers = soup.find_all("td")
+            if len(onlinePlayers) > 0:
+                print(str(len(onlinePlayers)/3) + " players")
+                playsound('audio.mp3', False)
+                var = input(getTime()+" Please ENTER to stop searching or other key to continue: ")
+                if len(var) == 0:
+                    print("STOPPING")
+                    break
+                else:
+                    time.sleep(30)
+                    continue
             else:
-                time.sleep(30)
+                print(getTime(), "Nobody is playing on this server")
+                time.sleep(60)
                 continue
-        else:
-            print("Nobody is playing on this server")
-            time.sleep(60)
-            continue
-        # To handle exceptions
+            # To handle exceptions
+        except:
+            print("Internet desconnected?")
+            time.sleep(30)
+            check()
 
 
+
+check()
