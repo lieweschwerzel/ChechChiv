7f4ede0dd6b9af3a36372cbf1a251b5ad527c4be
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2021-06-24 18:20:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,42 +5,48 @@ from bs4 import BeautifulSoup
 from playsound import playsound
 from datetime import datetime
 
+SLEEPTIME = 58
 
-def getTime():
+
+def checkserver():
+    while True:
+        try:
+            htmldata = getdata("https://refactor.jp/chivalry/?serverId=1491190")
+            soup = BeautifulSoup(htmldata, 'html.parser')
+            data = ''
+            for data in soup.find_all("tt"):
+                players = data.get_text()[1:3]
+
+            if players[0:1] != "0":
+                playsound('audio.mp3', False)
+                userInput = input(
+                    gettime() + " " + players + " players found! Please ENTER to stop searching or other key to search again: ")
+                if len(userInput) == 0:
+                    print("STOPPING")
+                    break
+                else:
+                    continue
+            else:
+                print(gettime(), "Nobody is playing on this server")
+                time.sleep(SLEEPTIME)
+                continue
+            # To handle exceptions
+        except:
+            print("Internet disconnected?")
+            time.sleep(SLEEPTIME)
+            checkserver()
+
+
+def gettime():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     return current_time
+
 
 # link for extract html data
 def getdata(url):
     r = requests.get(url)
     return r.text
 
-def check():
-    while True:
-        try:
-            htmldata = getdata("https://refactor.jp/chivalry/?serverId=1491190")
-            soup = BeautifulSoup(htmldata, 'html.parser')
-            onlinePlayers = soup.find_all("td")
-            if len(onlinePlayers) > 0:
-                print(str(len(onlinePlayers)/3) + " players")
-                playsound('audio.mp3', False)
-                var = input(getTime()+" Player(s) found! Please ENTER to stop searching or other key to continue: ")
-                if len(var) == 0:
-                    print("STOPPING")
-                    break
-                else:
-                    time.sleep(30)
-                    continue
-            else:
-                print(getTime(), "Nobody is playing on this server")
-                time.sleep(60)
-                continue
-            # To handle exceptions
-        except:
-            print("Internet disconnected?")
-            time.sleep(30)
-            check()
 
-
-check()
+checkserver()
