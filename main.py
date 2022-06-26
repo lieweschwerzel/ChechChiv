5270a1158d78641fbeea349fd608ff21db2a3357
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2022-06-26 18:52:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # import module
+import webbrowser
+from email.policy import default
 import os
 import sys
 import time
-from tkinter import END
+from tkinter import *
 from tkinter.constants import N
 from tkinter.messagebox import showinfo
 from tkinter.simpledialog import askstring
+from turtle import bgcolor, title, width
 import winsound
 from playsound import playsound
 from winsound import PlaySound
@@ -16,16 +19,16 @@ from datetime import datetime
 import tkinter as tk
 import multiprocessing
 
-SLEEPTIME = 60000  # milliseconds
-server_id = "1495246"
 server = "https://refactor.jp/chivalry/?serverId="
 searching = True
+
 name = "Sandclusterfck 1.X in Finland"
 file = os.path.join(getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__))),'audio.mp3')
 
 
 def check_players():
     global players
+    server_id = e.get()
     htmldata = get_data(server+server_id)
     soup = BeautifulSoup(htmldata, 'html.parser')
     get_server_name(soup)
@@ -68,8 +71,8 @@ def stop():
     winsound.PlaySound(None, winsound.SND_FILENAME)
     global searching
     searching = False
-    window.title("Check Chiv Server: " + server_id)
-    btn_open.config(text="Run Search", command=restart_server)
+    window.title("Check Chiv Server: " + e.get())
+    btn_runstop.config(text="Run Search", command=restart_server)
     txt_edit.insert(tk.END, get_time() + " " + "Stopped search \n")
     txt_edit.see(tk.END)
 
@@ -78,8 +81,9 @@ def playsound():
     winsound.PlaySound('SystemHand', winsound.SND_ASYNC + winsound.SND_LOOP)
 
 
-def update():    
-    btn_open.config(text="Stop", command=stop)
+def update():
+    sleeptime = e_minutes.get()    
+    btn_runstop.config(text="Stop", command=stop)
     playercheck = check_players()
     if playercheck > 1:
         txt_edit.insert(tk.END, get_time() + " " + players + " players online\n")
@@ -89,9 +93,9 @@ def update():
         playsound()
     else:
         txt_edit.insert(tk.END, get_time() + " Nobody is playing on this server" + "\n")    
-    window.title("Searching: " + server_id + " " + name)
+    window.title("Searching: " + e.get() + " " + name)
     txt_edit.see(tk.END) #keep scolling to END in window
-    window.after(SLEEPTIME, check_server)  # run  again after xxx ms
+    window.after((int(sleeptime)*60000), check_server)  # run  again after xxx ms
 
 
 def showinfo():    
@@ -104,32 +108,43 @@ def showinfo():
     searching = True
     check_server()
 
+def callback(event):
+    webbrowser.open_new(event.widget.cget("text"))
 
 
 window = tk.Tk()
-window.title("Check Chiv Server: " + server_id)
 
-window.geometry("450x100")
-window.rowconfigure(0, minsize=200, weight=1)
-window.columnconfigure(1, minsize=300, weight=1)
+
+window.geometry("370x230")
+#window.configure(bg = "black")
 
 scrollbar = tk.Scrollbar(window)
-label = tk.Label(window)
-txt_edit = tk.Text(window)
+label = tk.Label(window, text="min(s)")
+txt_edit = tk.Text(window, height=10, width=45)
 fr_buttons = tk.Frame(window)
-btn_open = tk.Button(fr_buttons, text="Run Search", command=check_server)
-btn_save = tk.Button(fr_buttons, text="Change Server", command=showinfo)
+btn_runstop = tk.Button(fr_buttons, text="Run Search", command=check_server)
+e = Entry(fr_buttons, justify='center', width=11)  
+e.insert(END, '1495246')
+e_minutes = Entry(window, justify='center', width=3)  
+e_minutes.insert(END, 1)
+window.title("Check Chiv Server: " + e.get())
 
-window.rowconfigure(0, minsize=100, weight=1)
-btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_save.grid(row=1, column=0, sticky="ew", padx=5)
-fr_buttons.grid(row=0, column=0, sticky="ns")
-txt_edit.grid(row=0, column=1, sticky="nsew")
-scrollbar.grid(row=0, column=3, rowspan=2,  sticky="nsew")
-label.grid(row=2, column=1, sticky="nsew")
+lbl = tk.Label(window, text=r"https://refactor.jp/chivalry/?country=CONTINENT_EU", fg="grey", cursor="hand2")
+lbl.grid(row=2, column=0)
+lbl.bind("<Button-1>", callback)
+
+window.columnconfigure(0, weight=1)
+fr_buttons.grid(row=0, column=0, padx=10, pady=5, sticky=E)
+e.grid(row=0, column=0, padx=5)
+btn_runstop.grid(row=0, column=1, padx=5, pady=5)
+e_minutes.grid(row= 0, column=0, padx=45, sticky=W)
+txt_edit.grid(row=1, column=0, padx=10, sticky=W)
+scrollbar.grid(row=1, column=1, rowspan=1)
+label.grid(row=0, column=0, sticky=W)
 
 txt_edit.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=txt_edit.yview)
+check_server()
 
 window.mainloop()
 
